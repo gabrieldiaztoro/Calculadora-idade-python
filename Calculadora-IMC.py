54a5f7f47e2a92d131d99c31c0669ae920e5e8d5
gabrieldiaztoro <gdiaz_toro@hotmail.com>
2022-09-23 22:22:32 -0300

    add texto calculadora e add de idade

--- Calculadora-IMC.py
+++ Calculadora-IMC.py
@@ -1,3 +1,4 @@
+from cProfile import label
 from tkinter import*
 from tkinter import ttk
 from turtle import width
@@ -7,21 +8,30 @@ janela.title ("Calculadora IMC - CSV19")
 janela.geometry ('310x400')
 
 # cores
-cor1 = "#333333"
-cor2 = "#3b3b3b"
+cor1 = "#333333" #--cinza escuro
+cor2 = "#3B3B3B" #--cinza
+cor3 = "#FFFFFF" #--branca
+cor4 = "#FF8C00" #--laranja
+
 #-------criando frames----
-frame_topo = Frame(janela, width=310, height=150, pady=0, padx=0, relief=FLAT, bg=cor1) 
+frame_top = Frame(janela, width=310, height=150, pady=0, padx=0, relief=FLAT, bg=cor1) 
 #- parte de cima da calculadora relief = estilo   'flat' ou FLAT, bg = background
-frame_topo.grid(row=0, column=0)
+frame_top.grid(row=0, column=0)
 
-frame_baixo = Frame(janela, width=310, height=300, pady=0, padx=0, relief=FLAT, bg=cor2) 
+frame_down = Frame(janela, width=310, height=300, pady=0, padx=0, relief=FLAT, bg=cor2) 
+#fg = cor letra
 #- parte de cima da calculadora relief = estilo   'flat' ou FLAT, bg = background
-frame_baixo.grid(row=1, column=0)
+frame_down.grid(row=1, column=0)
 
 
+#----criando label para frame (Top)
 
+l_calculadora = Label(frame_top, text="CALCULADORA", width=25, height=1, padx=3, relief='flat', anchor='center', font=('Ivi 15 bold'), bg=cor1 , fg=cor3)
+l_calculadora.place(x=0, y=30)
 
+l_idade = Label(frame_top, text="DE IDADE", width=11, height=1, padx=0, relief='flat', anchor='center', font=('Arial 35 bold'), bg=cor1 , fg=cor4)
+l_idade.place(x=-0, y=70)
 
-
+#----criando label para frame (Down)
 
 janela.mainloop()
