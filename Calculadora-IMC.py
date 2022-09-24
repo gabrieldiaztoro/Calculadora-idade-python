19de7fb8d46deb4e13fa7a96f8ac2448c19369a8
gabrieldiaztoro <gdiaz_toro@hotmail.com>
2022-09-23 23:29:12 -0300

    inserindo o calendario

--- Calculadora-IMC.py
+++ Calculadora-IMC.py
@@ -2,6 +2,7 @@ from cProfile import label
 from tkinter import*
 from tkinter import ttk
 from turtle import width
+from tkcalendar import Calendar, DateEntry
 
 janela =Tk()
 janela.title ("Calculadora IMC - CSV19")
@@ -34,4 +35,14 @@ l_idade.place(x=-0, y=70)
 
 #----criando label para frame (Down)
 
+l_data_inicial = Label(frame_down, text="Data de hoje", width=25, height=1, padx=0, pady=0, relief='flat', anchor=NW, font=('Ivi 11 bold'), bg=cor2 , fg=cor3) #anchor=NM = north west
+l_data_inicial.place(x=30, y=30)
+
+l_data_nascimento = Label(frame_down, text="Data de nascimento", width=25, height=1, padx=0, pady=0, relief='flat', anchor=NW, font=('Ivi 11 bold'), bg=cor2 , fg=cor3)
+l_data_nascimento.place(x=30, y=60)
+
+#calendario
+
+cal_hoje = DateEntry(frame_down, width=13, bg='darkblue', fg=cor3, borderwidth=2, date_patter= mm/dd/y, y=2022)
+cal_hoje.place(x=170, y=30)
 janela.mainloop()
