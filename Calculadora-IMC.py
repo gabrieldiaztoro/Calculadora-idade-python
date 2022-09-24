3ee688e417488c0847d79d9d8c4186f256251061
gabrieldiaztoro <gdiaz_toro@hotmail.com>
2022-09-23 23:56:19 -0300

    finalizando o projeto

--- Calculadora-IMC.py
+++ Calculadora-IMC.py
@@ -36,13 +36,22 @@ l_idade.place(x=-0, y=70)
 #----criando label para frame (Down)
 
 l_data_inicial = Label(frame_down, text="Data de hoje", width=25, height=1, padx=0, pady=0, relief='flat', anchor=NW, font=('Ivi 11 bold'), bg=cor2 , fg=cor3) #anchor=NM = north west
-l_data_inicial.place(x=30, y=30)
+l_data_inicial.place(x=20, y=30)
 
 l_data_nascimento = Label(frame_down, text="Data de nascimento", width=25, height=1, padx=0, pady=0, relief='flat', anchor=NW, font=('Ivi 11 bold'), bg=cor2 , fg=cor3)
-l_data_nascimento.place(x=30, y=60)
+l_data_nascimento.place(x=20, y=70)
 
 #calendario
 
-cal_hoje = DateEntry(frame_down, width=13, bg='darkblue', fg=cor3, borderwidth=2, date_patter= mm/dd/y, y=2022)
-cal_hoje.place(x=170, y=30)
+cal_hoje = DateEntry(frame_down, width=13, bg='darkblue', fg=cor3, borderwidth=2, date_patter= 'dd/mm/y', y=2022)
+cal_hoje.place(x=180, y=30)
+
+cal_nascimento = DateEntry(frame_down, width=13, bg='darkblue', fg=cor3, borderwidth=2, date_patter= 'dd/mm/y', y=2022)
+cal_nascimento.place(x=180, y=70)
+
+#resultado
+l_resultado_anos = Label(frame_down, text="27", width=25, height=1, padx=0, pady=0, relief='flat', anchor=NW, font=('Ivi 11 bold'), bg=cor2 , fg=cor3)
+l_resultado_anos.place(x=20, y=70)
+
+
 janela.mainloop()
